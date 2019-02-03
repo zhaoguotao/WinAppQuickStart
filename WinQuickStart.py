07d8038a50ab7267255fa361bfeb27d12b64bcce
zhaoguotao <505541114@qq.com>
2019-02-03 15:57:23 +0800

    新增截图工具、cmd、网络和共享中心、程序和功能、文件夹选项等快捷按钮。

--- WinQuickStart.py
+++ WinQuickStart.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 '''
 os.popen(cmd) vs os.system(cmd):
-os.popen(cmd)无法再纯GUI程序中使用，就是说只能用pyinstaller -F -c 生成exe，如果用-w，则os.popen(cmd)命令无法执行。而os.system(cmd)可以使用-w编译成exe。
+os.popen(cmd)无法在纯GUI程序中使用，就是说只能用pyinstaller -F -c 生成exe，如果用-w，则os.popen(cmd)命令无法执行。而os.system(cmd)可以使用-w编译成exe。
 '''
 import os
 import time
@@ -85,6 +85,28 @@ def Callback_mmsys():
     cmd = "mmsys.cpl"#sound
     os.startfile(cmd)
     
+def Callback_appwiz():
+    cmd = "appwiz.cpl"
+    os.startfile(cmd)
+    
+def Callback_controlfolders():
+    cmd = "control folders"
+#     os.startfile(cmd)
+    os.system(cmd)
+    
+def Callback_NetworkAndSharingCenter():
+    cmd = "control.exe /name Microsoft.NetworkAndSharingCenter"
+    os.system(cmd)
+    
+def Callback_snippingtool():
+    cmd = "snippingtool.exe"
+    os.startfile(cmd)
+   
+def Callback_cmd():
+    cmd = "cmd.exe"
+    os.startfile(cmd)
+    
+     
 def Callback_attributes():
     ret = top.wm_attributes("-topmost")
     if ret == 1:
@@ -120,13 +142,15 @@ btn_names_2 = ["控制面板","资源管理器 ","Internet属性 "]
 btn_names_3 = ["鼠标属性","系统信息 ","画图 "]
 btn_names_4 = ["远程桌面连接","网络连接 ","打开记事本 "]
 btn_names_5 = ["注册表 ","系统属性 ","关于Windows"]
-btn_names_6 = ["声音","reserved1","reserved2"]
+btn_names_6 = ["声音","程序和功能","文件夹选项"]
+btn_names_7 = ["截图工具","cmd","网络和共享中心"]
 btn_names.append(btn_names_1)
 btn_names.append(btn_names_2)
 btn_names.append(btn_names_3)
 btn_names.append(btn_names_4)
 btn_names.append(btn_names_5)
 btn_names.append(btn_names_6)
+btn_names.append(btn_names_7)
 
 btn_func=[]
 btn_func_1  = [Callback_devmgmt,Callback_taskmgr,Callback_calc]
@@ -134,13 +158,15 @@ btn_func_2  = [Callback_control,Callback_explorer,Callback_inetcpl]
 btn_func_3  = [Callback_Mouse,Callback_msinfo32,Callback_mspaint]
 btn_func_4  = [Callback_mstsc,Callback_networkLink,Callback_notepad]
 btn_func_5  = [Callback_regedit,Callback_systemAttr,Callback_winver]
-btn_func_6  = [Callback_mmsys,None,None]
+btn_func_6  = [Callback_mmsys,Callback_appwiz,Callback_controlfolders]
+btn_func_7  = [Callback_snippingtool,Callback_cmd,Callback_NetworkAndSharingCenter]
 btn_func.append(btn_func_1)
 btn_func.append(btn_func_2)
 btn_func.append(btn_func_3)
 btn_func.append(btn_func_4)
 btn_func.append(btn_func_5)
 btn_func.append(btn_func_6)
+btn_func.append(btn_func_7)
 
 if __name__ == '__main__':
     top = Tk()
@@ -151,13 +177,14 @@ if __name__ == '__main__':
     	top.attributes("-toolwindow", 1)#Toolbar style，this Property is only for windows，linux can not use
     top.attributes("-topmost", 1)#top
     top.attributes("-alpha",1)#transparency
-
+    m = len(btn_names)
     for i in range(len(btn_names)):#row
         for j in range(len(btn_names[i])):#column
             ttk.Button(top, text =btn_names[i][j], command = btn_func[i][j],width=13).grid(row=i,column=j, ipady=2)
+            
     BTN1 = ttk.Button(top, text ="置顶: ON", command = Callback_attributes,width=13)
-    BTN1.grid(row=6,column=0, ipady=2)
-    ttk.Button(top, text ="本程序路径", command = Callback_OpenAppFolder,width=13).grid(row=6,column=1, ipady=2)
-    ttk.Button(top, text ="显示标题", command = Callback_overrideredirect,width=13).grid(row=6,column=2, ipady=2)
+    BTN1.grid(row=m+1,column=0, ipady=2)
+    ttk.Button(top, text ="本程序路径", command = Callback_OpenAppFolder,width=13).grid(row=m+1,column=1, ipady=2)
+    ttk.Button(top, text ="显示标题", command = Callback_overrideredirect,width=13).grid(row=m+1,column=2, ipady=2)
     top.bind('<B3-Motion>',movewindow)
     top.mainloop()
